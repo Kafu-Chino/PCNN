bf4ccb0947638b816a6659c8f69a35170ce4a4c7
Kafu-Chino <huzf1218@gmail.com>
2020-03-22 22:33:05 +0800

    户中方-项目初建

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2020/3/22 22:29
+# Author : Hu
+# File : config.py
+
+# 写一些配置
---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -- coding: utf-8 --
+# @Time : 2020/3/22 22:29
+# Author : Hu
+# File : main.py
+
+# 实验主函数
